84624cf03c2e916dcc698822187de703c5f6ddea
pdellecase <dev@dellecase.com>
2020-04-05 22:30:08 +0200

    Fixing Lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 import { API } from "aws-amplify";
 import "./Home.css";
 
